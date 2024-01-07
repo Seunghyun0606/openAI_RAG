0c7b1e3cc9d1fcff908654227fa57d6afd9b2ae6
seungHyunIm <sh.im993366@gmail.com>
2024-01-08 04:42:32 +0900

    pdf 으로부터 raw_text 추출

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ import os
 from dotenv import load_dotenv
 import openai
 from openai import OpenAI
+from PYPDF2 import PdfReader
 
 # env에서 Open API 키 가져오기
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 openai.api_key = OPENAI_API_KEY 
 
+def get_pdf_text(file_path):
+    # pypdf 를 통해서 pdf 문서 읽기
+    doc_reader = PdfReader(file_path)
+    raw_text = ''
+    for i, page in enumerate(doc_reader.pages):
+        text = page.extract_text()
+        if text:
+            raw_text += text
+    return raw_text
 
 
 def main():
@@ -16,6 +26,9 @@ def main():
     current_path = os.getcwd()
     file_name = 'test.pdf'
     file_path = os.path.join(current_path, file_name)
+
+    # Embedding 할 문서에서 Text 추출
+    raw_text = get_pdf_text(file_path)
 
 
 if __name__ == '__main__':
